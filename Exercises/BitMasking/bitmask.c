e1b8ce998584ee3ca3d2e0824e364c93fe2dbe62
ReubenMathew <intelphysic@gmail.com>
2019-07-23 22:09:39 -0500

    started MIPS and C exercises

---
+++ Exercises/BitMasking/bitmask.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(){
+
+	unsigned int a = 17;
+	printf("%d",a);
+	
+	unsigned int mask;
+
+	return 0;
+}
---
+++ Exercises/BitOperators/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+	unsigned int a = 15; /* 0000 1111 */
+	unsigned int b = 30; /* 0001 1110 */
+
+	int c = 0;
+
+	c = a & b;
+	printf("AND %d\n",c);
+	c = a | b;
+	printf("OR %d\n",c);
+
+	c = ~a;
+	printf("COMPLEMENT %d\n", c);
+
+	c = a ^ b;
+	printf("XOR %d\n",c);
+
+	c = a >> 2;
+	printf("BIT SHIFT RIGHT %d\n",c);
+
+	c = a << 2;
+	printf("BIT SHIFT LEFT %d\n",c);
+
+
+	return 0;
+
+}
